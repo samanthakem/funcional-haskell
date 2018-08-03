32cf9af0a5f41de161ec839efb60fb0b5219d94f
Samantha Monteiro <samanthakem@gmail.com>
2018-08-03 15:30:17 -0300

    Adding question 4

---
+++ q4.hs
@@ -0,0 +1,30 @@
+expSearch arr x = do
+  let size = (length arr)
+  i <- expSearch' arr size x
+  return i
+
+expSearch' arr n x = do
+  if ((arr !! 0)  == x)
+    then return 0
+  else do
+    binSearch arr (((auxUpdateValue 1 arr x n) `div` 2)) (minBetween (auxUpdateValue 1 arr x n) n) x
+
+binSearch array left right element = do
+  if (right >= left) then do
+    let subRightLeft = right - left
+    let auxMiddle = subRightLeft `div` 2
+    let mid = left + auxMiddle
+
+    if ((array !! mid) == element) then do
+      return mid
+    else do
+      if ((array !! mid) > element) then do
+        binSearch array left (mid-1) element
+      else do
+        binSearch array (mid+1) right element
+  else do
+    return (-1)
+
+minBetween x y | x < y = x | otherwise = y
+
+auxUpdateValue i arr x n | i < n && ((arr !! i) <= x) = auxUpdateValue (i*2) arr x n | otherwise = i
